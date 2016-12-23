357579eb8877e868a43f7ea3f905336c64e78468
Fauzi Aulia <Fauzi Aulia>
2016-12-23 15:53:42 +0700

    always put MP products last

--- app/code/local/Tinkerlust/PositionRandomizer/Model/Resource/Categoryproduct.php
+++ app/code/local/Tinkerlust/PositionRandomizer/Model/Resource/Categoryproduct.php
@@ -21,15 +21,22 @@ class Tinkerlust_PositionRandomizer_Model_Resource_Categoryproduct extends Mage_
 	{
 		$write = Mage::getSingleton('core/resource')->getConnection('core_write');
 		$table = $this->getMainTable();
-		$column = 'position';
+		$table2= Mage::getSingleton('core/resource')->getTableName('catalog/product');
 		$min_number = 100000;
 		$max_random_value = $num_of_products > $min_number ? $num_of_products : $min_number;
 
-		$query = "update $table set $column = CAST(RAND() * $max_random_value AS UNSIGNED)";
+		//$query = "update $table set $column = CAST(RAND() * $max_random_value AS UNSIGNED)";
+
+		$query = "update $table c join $table2 p on c.product_id = p.entity_id 
+					set c.position = CAST(RAND() * $max_random_value + 5000 AS UNSIGNED) where p.sku not like '5%';";
+
+		$query2 = "update $table c join $table2 p on c.product_id = p.entity_id 
+					set c.position = CAST(RAND() * 5000 AS UNSIGNED) where p.sku like '5%';";
 
 		try 
 		{
 			$write->query($query);
+			$write->query($query2);
 			return true;
 		} 
 		catch (Exception $e) 
